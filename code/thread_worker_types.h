d0b07bc23593584c1f739b4ee9b407674f732434
El <47612469+204870@users.noreply.github.com>
2024-02-27 10:55:20 -0500

    Update thread_worker_types.h
    the tcb

--- code/thread_worker_types.h
+++ code/thread_worker_types.h
@@ -5,14 +5,27 @@
 
 typedef unsigned int worker_t;
 
+typedef enum {
+    READY,
+    SCHEDULED,
+    BLOCKED,
+    TERMINATED,
+    EXECUTE
+} ThreadStatus;
+
 typedef struct TCB
 {
     /* add important states in a thread control block */
     // thread Id
+    uint32_t tid;
     // thread status
+    ThreadStatus status;
     // thread context
+    ucontext_t context;
     // thread stack
+    char stack[STACK_SIZE]
     // thread priority
+    int priority;
     // And more ...
 
     // YOUR CODE HERE
