de64c0d9e889c10ad268c6032cd9e1c86b467525
El <47612469+204870@users.noreply.github.com>
2024-03-03 01:37:53 -0500

    worker yield (impllementer timer)

--- code/thread-worker.c
+++ code/thread-worker.c
@@ -54,9 +54,11 @@ int worker_yield()
 {
 
     // - change worker thread's state from Running to Ready
-  new_tcb->status = READY;
+    current_tcb->status = READY;
     // - save context of this thread to its thread control block
+    ucontext_t current_context = current_tcb->context;
     // - switch from thread context to scheduler context
+    swapcontext(&current_context, &scheduler_context)
     return 0;
 
 };
@@ -66,9 +68,10 @@ void worker_exit(void *value_ptr)
 {
     // - if value_ptr is provided, save return value
     // - de-allocate any dynamic memory created when starting this thread (could be done here or elsewhere)
-   if (*value_ptr != NULL){
-    free(value_ptr)
-  } //or something like that
+
+    if (*value_ptr != NULL){
+        free(value_ptr);
+    } //or something like that
 }
 
 /* Wait for thread termination */
